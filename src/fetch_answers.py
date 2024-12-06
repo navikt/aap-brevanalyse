69f7b01e67e44ca9ec42fceb813dfbc628318990
Anders Lauvland <anders.lauvland@nav.no>
2024-12-06 14:01:19 +0100

    Fjernet if-test

--- src/fetch_answers.py
+++ src/fetch_answers.py
@@ -5,30 +5,19 @@ import os
 
 # %%
 
-if os.path.exists(".env"):
-    load_dotenv()
-    email = os.getenv("ta_email")
-    password = os.getenv("ta_password")
-    survey_id = os.getenv("ta_survey_id")
 
-# %%
-
-else:
-    import utils_secrets
-
-    hente_hemmeligheter("TaskAnalytics")
-    email = os.getenv("user")
-    password = os.getenv("password")
-    survey_id = os.getenv("survey_id")
+load_dotenv()
+email = os.getenv("ta_email")
+password = os.getenv("ta_password")
+survey_id = os.getenv("ta_survey_id")
 
 
 # %%
-get_survey = task.download_survey(
-    username=email,
-    password=password,
-    survey_id=survey_id,
-    filename_path="data/new/survey.csv",
-)
+get_survey = task.download_survey(username=email,
+                                  password=password,
+                                  survey_id=survey_id,
+                                  filename_path="data/new/survey.csv"
+                                  )
 
 
 get_survey.status_code
