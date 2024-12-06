f5192965b66313e69361ff6e5ef52c7a08600f4c
Anders Lauvland <anders.lauvland@nav.no>
2024-12-06 09:29:36 +0100

    Oppdatert fetch_answers.py for å ta inn miljøvariabler med google secrets

--- src/fetch_answers.py
+++ src/fetch_answers.py
@@ -2,15 +2,32 @@
 import taskanalytics_data_wrapper.taskanalytics_api as task
 from dotenv import load_dotenv
 import os
+
 # %%
-load_dotenv()
+
+if os.path.exists('.env'):
+    load_dotenv()
+    email = os.getenv("ta_email")
+    password = os.getenv("ta_password")
+    survey_id = os.getenv("ta_survey_id")
+
 # %%
-email = os.getenv("ta_email")
-password = os.getenv("ta_password")
-survey_id = os.getenv("ta_survey_id")
+
+else:
+    import utils_Secrets
+    hente_hemmeligheter("TaskAnalytics")
+    email = os.getenv("user")
+    password = os.getenv("password")
+    survey_id = os.getenv("survey_id")
+
+
 # %%
-get_survey = task.download_survey(
-    username=email, password=password, survey_id=survey_id, filename_path="data/new/survey.csv"
-)
+get_survey = task.download_survey(username=email,
+                                  password=password,
+                                  survey_id=survey_id, 
+                                  filename_path="data/new/survey.csv"
+  )
+
+
 get_survey.status_code
 # %%
